b640a4eae944544b683bd03a90bc939ea634b5bd
thanglongsp <thanglongnguyensp@gmail.com>
2019-04-14 20:32:21 +0700

    shutdown thread

--- BTVN_TrungNT9/src/main/java/BAI3/BAI1/ThreadRead.java
+++ BTVN_TrungNT9/src/main/java/BAI3/BAI1/ThreadRead.java
@@ -3,29 +3,37 @@ package BAI3.BAI1;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ThreadRead extends Thread {
     Socket socket = null;
     String name;
+    private final AtomicBoolean running = new AtomicBoolean(true);
 
     public ThreadRead(Socket socket, String name) {
         this.socket = socket;
         this.name = name;
     }
 
+    public void shutdown() throws InterruptedException {
+        ThreadRead.sleep(1000);
+        running.set(false);
+    }
+
     @Override
     public void run() {
-        while (true) {
+        while (running.get()) {
             DataInputStream is = null;
             try {
                 is = new DataInputStream(socket.getInputStream());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
                 System.out.println("From " + name + ":" + is.readUTF());
             } catch (IOException e) {
                 e.printStackTrace();
+                try {
+                    shutdown();
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
             }
         }
     }
--- BTVN_TrungNT9/src/main/java/BAI3/BAI1/ThreadWrite.java
+++ BTVN_TrungNT9/src/main/java/BAI3/BAI1/ThreadWrite.java
@@ -4,32 +4,38 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ThreadWrite extends Thread {
     Socket socket = null;
     String name;
-    boolean isRun = true;
+    private final AtomicBoolean running = new AtomicBoolean(true);
 
     public ThreadWrite(Socket socket, String name) {
         this.socket = socket;
         this.name = name;
     }
 
+    public void shutdown() throws InterruptedException {
+        ThreadRead.sleep(1000);
+        running.set(false);
+    }
+
     @Override
     public void run() {
-        while (isRun) {
+        while (running.get()) {
             DataOutputStream os = null;
             try {
                 os = new DataOutputStream(socket.getOutputStream());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
                 os.writeUTF(new Scanner(System.in).nextLine());
             } catch (IOException e) {
+                try {
+                    shutdown();
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
                 e.printStackTrace();
             }
-
         }
     }
 }
